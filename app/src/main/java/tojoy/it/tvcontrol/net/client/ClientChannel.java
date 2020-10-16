d6e608715a14f4aadb028ea34390983fdd1d2a0d
宋明站 <songmingzhan@tojoy.com>
2020-10-16 10:15:22 +0800

    问题:测试提交 原因: 解决方案：

---
+++ app/src/main/java/tojoy/it/tvcontrol/net/client/ClientChannel.java
@@ -0,0 +1,13 @@
+package tojoy.it.tvcontrol.net.client;
+
+/**
+ * <pre>
+ *     author : songmingzhan
+ *     e-mail : songmingzhan@tojoy.com
+ *     time   : 2020/10/16
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class ClientChannel {
+}
